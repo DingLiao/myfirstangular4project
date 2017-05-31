0e76b9cb4db60268dbf8c751302d02f09b6c9721
Ding Liao <ding.liao@in4mo.com>
2017-05-31 16:47:52 +0300

    add http connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent }  from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -13,6 +19,8 @@ import { HeroService } from './hero.service';
   imports:      [ 
   	BrowserModule,
   	FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ 
